0fa94a0371138ff98c159cbfa1c713567bb9f688
canhhuy116 <canhhuy1162002@gmail.com>
2024-01-11 23:56:50 +0700

    chore: update for between validation example

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,4 @@
+import { Between } from './validator/BetweenDecorator';
 import { ValidationFacade } from './ValidationFacade';
 import { MustMatch } from './validator/MustMatchDecorator';
 import { NotNullOrEmpty } from './validator/NotNullOrEmptyDecorator';
@@ -12,18 +13,29 @@ class MustMatchClass {
   input: string;
 }
 
+class BetweenClass {
+  @Between([0, 100])
+  input: number;
+}
+
 const validators = {
   NotNullOrEmpty: new NotNullOrEmptyClass(),
   MustMatch: new MustMatchClass(),
+  Between: new BetweenClass(),
 };
 
-export function validate(selectedValidator: string, inputValue: string, regex?: RegExp) {
+export function validate(selectedValidator: string, inputValue: string, regex?: RegExp, range?: number[]) {
   // Get the selected validator function
   let model = validators[selectedValidator];
 
   if (selectedValidator === 'MustMatch' && regex) {
     model = new MustMatchClass();
     Reflect.defineMetadata('Validation:MustMatch', regex, model, 'input');
+  }
+
+  if (selectedValidator === 'Between' && range) {
+    model = new BetweenClass();
+    Reflect.defineMetadata('Validation:Between', range, model, 'input');
   }
 
   model.input = inputValue;
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { validate } from './example';
   const selectedValidator = (document.getElementById('validatorSelect') as HTMLSelectElement).value;
   const inputValue = (document.getElementById('inputValue') as HTMLInputElement).value;
   const regexInput = (document.getElementById('regexInput') as HTMLInputElement).value;
+  const minValueInput = (document.getElementById('minValue') as HTMLInputElement).value;
+  const maxValueInput = (document.getElementById('maxValue') as HTMLInputElement).value;
   const messageElement = document.getElementById('message') as HTMLParagraphElement;
 
   const regex = selectedValidator === 'MustMatch' ? new RegExp(regexInput) : undefined;
-  const msg = validate(selectedValidator, inputValue, regex);
+  const minMax = selectedValidator === 'Between' ? [Number(minValueInput), Number(maxValueInput)] : undefined;
+  const msg = validate(selectedValidator, inputValue, regex, minMax);
 
   messageElement.innerText = msg;
 };
@@ -16,10 +19,16 @@ import { validate } from './example';
 (window as any).toggleRegexInput = function () {
   const selectedValidator = (document.getElementById('validatorSelect') as HTMLSelectElement).value;
   const regexInput = document.getElementById('regexInput') as HTMLInputElement;
+  const betweenInputs = document.getElementById('betweenInputs') as HTMLDivElement;
 
   if (selectedValidator === 'MustMatch') {
     regexInput.style.display = 'block';
+    betweenInputs.style.display = 'none';
+  } else if (selectedValidator === 'Between') {
+    regexInput.style.display = 'none';
+    betweenInputs.style.display = 'flex';
   } else {
     regexInput.style.display = 'none';
+    betweenInputs.style.display = 'none';
   }
 };
