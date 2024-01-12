a5b574dce84c99791e371f79571638260b018940
Tranlehieu <66514357+Tranlehieu@users.noreply.github.com>
2024-01-12 10:53:44 +0700

    Fix Between validator bug

--- src/validator/BetweenDecorator.ts
+++ src/validator/BetweenDecorator.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 
-export function Between(range: [number, number]) {
-  console.log(range);
+export function Between(range: [lower: any, upper: any]) {
   return (target: Object, propertyKey: string) =>
     Reflect.defineMetadata("Validation:Between", range, target, propertyKey);
 }
